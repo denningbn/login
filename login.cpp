a6071032fafb8d82896de2f9058625f6f9d43f52
denningbn <denningbn@appstate.edu>
2023-09-30 14:18:08 +0000

    changed makefile to have -g flag for debugging, and added malloc usage to login, where I'm also comparing to 'admin\n' a null terminated string

--- login.cpp
+++ login.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <string.h>
+
+
+#define MAX_INPUT_LENGTH 16
+
+bool valid_username(char* username);
+	
 
 int main() {
-	std:: cout << "Hello world!";
+	bool good_username = 0;
+
+	char *input = (char*)malloc(MAX_INPUT_LENGTH * sizeof(char));
+
+	while(!good_username){
+		std::cout << "Please enter username:";
+
+		fgets(input, MAX_INPUT_LENGTH, stdin);
+
+		good_username = valid_username(input);
+	}
+
+	free(input);
 
 	return 0;
+}
+
+bool valid_username(char* username){
+	const char* good_name = "admin\n";
+
+	bool r_val = strcmp(good_name, username);
+	return !r_val;
 }
--- makefile
+++ makefile
@@ -1,7 +1,10 @@
 CC=g++
 
 login: login.o
-	$(CC) -o login login.o
+	$(CC) -g -o login login.o
+
+login.o:
+	$(CC) -c -g login.cpp
 
 clean:
 	rm *.o login
